e8790f437327b2307fefbbfd24a9aaa962b0b82a
User Name <kauth333@gmail.com>
2023-02-26 17:29:10 -0500

    add use state and import interface an App.tsx for profile model

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // npm modules 
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 
 // page components
@@ -15,17 +15,19 @@ import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 
 // services
 import * as authService from './services/authService'
+import * as profileService from './services/profileService'
 
 // stylesheets
 import './App.css'
 
 // types
-import { User } from './types/models'
+import { User, Profile } from './types/models'
 
 function App(): JSX.Element {
   const navigate = useNavigate()
   
   const [user, setUser] = useState<User | null>(authService.getUser())
+  const [profiles, setProfiles] = useState<Profile[]>([])
 
   const handleLogout = (): void => {
     authService.logout()
@@ -54,7 +56,7 @@ function App(): JSX.Element {
           path="/profiles"
           element={
             <ProtectedRoute user={user}>
-              <Profiles />
+              <Profiles  profiles={profiles}/>
             </ProtectedRoute>
           }
         />
