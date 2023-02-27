4d8665653f8c9f4b096ccb98f9f77cddef0e4285
User Name <kauth333@gmail.com>
2023-02-27 00:28:37 -0500

    Create Alloutfits page

---
+++ src/pages/AllOutfits/AllOutfits.tsx
@@ -0,0 +1,26 @@
+// types
+import { Outfit } from '../../types/models'
+
+interface OutfitsProps {
+	outfits: Outfit[];
+}
+
+const AllOutfits = (props: OutfitsProps): JSX.Element => {
+  const { outfits } = props
+
+  if(!outfits.length) return <p>No outfits yet</p>
+
+  return (
+    <>
+      <h1>Hello. This is a list of all the Outfits.</h1>
+      {outfits.map((outfit: Outfit) =>
+        <p key={outfit.id}>
+        {outfit.description}
+        {outfit.photo}
+        </p>
+      )}
+    </>
+  )
+}
+ 
+export default AllOutfits
