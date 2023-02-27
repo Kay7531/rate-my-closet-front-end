62feb6ab0bb134172fcb57347b05c7f1142a49dc
User Name <kauth333@gmail.com>
2023-02-27 11:17:03 -0500

    Stub up New outfit form.

--- src/pages/NewOutfitForm/NewOutfitForm.tsx
+++ src/pages/NewOutfitForm/NewOutfitForm.tsx
@@ -1,10 +1,43 @@
+import { useState } from "react"
+import { Link, useNavigate } from 'react-router-dom'
+import styles from './NewOutfitForm.module.css'
+//Services
+import * as outfitService from '../../services/outfitService'
+//Types
+import { NewOutfitFormData, PhotoFormData, NewOutfitFormElements } from "../../types/forms"
 
+interface NewOutfitFormProps {
+    NewOutfitFormData: NewOutfitFormData
+}
 
-const NewOutfitForm = (): JSX.Element => {
+const NewOutfitForm = (props: NewOutfitFormData): JSX.Element => {
+const [form, setForm] = useState()
+const [photoData, setPhotoData] = useState({})
+const handleChange = ({}) => {
+
+}
+const handleSubmit = () => {
+
+}
+
+const handleChangePhoto = () => {
+    
+}
     return (
-        <>
-        <h1> Post Your outfit</h1>
-        </>
+        <main>
+            {/* <form onSubmit={{ handleSubmit }}
+           <label htmlFor="text-input"></label>
+            <input
+                required
+                type="text"
+                name="description"
+                id="description-input"
+                value={form.description}
+                placeholder="Location"
+                onChange={handleChange}
+            />
+        </form> */}
+        </main >
     )
 }
 
