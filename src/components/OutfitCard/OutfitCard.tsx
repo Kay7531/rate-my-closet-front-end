62baa2333b6a83e38b1eae5f208be5ed058fae17
User Name <kauth333@gmail.com>
2023-02-28 06:19:54 -0500

    Add update function to app.tsx

--- src/components/OutfitCard/OutfitCard.tsx
+++ src/components/OutfitCard/OutfitCard.tsx
@@ -1,6 +1,19 @@
-const OutfitCard = (): JSX.Element => {
+import { useState } from 'react'
+import styles from './OutfitCard.module.css'
+
+import {Outfit} from '../../types/models'
+interface OutfitCardProps {
+    outfit: Outfit
+}
+
+const OutfitCard = ({outfit}:OutfitCardProps): JSX.Element => {
     return(
         <>
+        <div className={styles.container}>
+        
+        <img src={outfit.photo} alt="" />
+        {outfit.description}
+        </div>
         </>
     )
 }
--- src/pages/AllOutfits/AllOutfits.tsx
+++ src/pages/AllOutfits/AllOutfits.tsx
@@ -1,28 +1,45 @@
+import * as outfitService from '../../services/outfitService'
+//Components
+import OutfitCard from '../../components/OutfitCard/OutfitCard';
+
 // types
 import { Outfit } from '../../types/models'
 
 interface OutfitsProps {
-	outfits: Outfit[];
+  outfits: Outfit[];
 }
 
 const AllOutfits = (props: OutfitsProps): JSX.Element => {
   const { outfits } = props
 
-  if(!outfits.length) return <p>No outfits yet</p>
+  if (!outfits.length) return <p>No outfits yet</p>
+
+  const handleDeleteOutfit = async (evt: React.MouseEvent, outfitId: number): Promise<void> => {
+    evt.preventDefault()
+    try {
+      await outfitService.deleteOutfit(outfitId)
+      
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
 
   return (
     <>
       <h1>Hello. This is a list of all the Outfits.</h1>
       {outfits.map((outfit: Outfit) =>
-        <p key={outfit.id}>
-        {outfit.description}
-        <img src={outfit.photo} alt="" />
-        </p>
-        
-        
-      )}
+      (<OutfitCard
+        key={outfit.id}
+        outfit={outfit} 
+        handleDeleteOutfit ={handleDeleteOutfit}
+        />
+
+
+
+      ))}
     </>
   )
 }
- 
+
 export default AllOutfits
