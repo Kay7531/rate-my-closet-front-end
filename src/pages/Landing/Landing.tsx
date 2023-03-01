e8e2b0f68a1d800f5728668afde1345780d5ebc1
User Name <kauth333@gmail.com>
2023-03-01 11:08:20 -0500

    Style the landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
 // stylesheets
 import styles from './Landing.module.css'
+
 
 // types
 import { User, Profile } from '../../types/models'
@@ -7,17 +9,21 @@ import { Profiler } from 'react';
 
 interface LandingProps {
   user: User | null;
-  profile: Profile | null;
+  
   
 }
 
 const Landing = (props: LandingProps): JSX.Element => {
-  const { user, profile} = props
+  const { user} = props
 
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
-      {/* <img src={profile.photo} alt="" /> */}
+      <div className={styles.about}>
+      <h1>Where style enthusiasts gather to show off and get opinions on their fits. </h1>
+      <h2>Get started here</h2>
+      <Link to= "/signup"><button style={{width:200, height:50, fontSize:20}}>Rate My Closet</button></Link>
+      </div>
+     <img style= {{width:550, height:550}}src="public/lfash-anding.png" alt="" />
     </main>
   )
 }
